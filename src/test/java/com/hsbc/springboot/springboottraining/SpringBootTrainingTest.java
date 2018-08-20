267001702c1052ca63940cf78631c9b929257793
wushuping <shup.wu@haihangyun.com>
2018-08-21 07:24:07 +0800

    test

--- src/test/java/com/hsbc/springboot/springboottraining/SpringBootTrainingTest.java
+++ src/test/java/com/hsbc/springboot/springboottraining/SpringBootTrainingTest.java
@@ -1,18 +1,19 @@
 package com.hsbc.springboot.springboottraining;
 
 
-import org.assertj.core.groups.FieldsOrPropertiesExtractor;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.hsbc.springboot.springboottraining.controller.FileManagerController;
 import com.hsbc.springboot.springboottraining.entity.FileEntity;
+import com.hsbc.springboot.springboottraining.repository.FileRepository;
 import com.hsbc.springboot.springboottraining.service.FileService;
 /**
  * @Description test
@@ -27,6 +28,13 @@ public class SpringBootTrainingTest {
 
 	 @Autowired  
 	 FileService fileService;  
+	 
+	 @Autowired 
+	 FileManagerController fileManagerController;
+	 
+	 @Autowired
+	 FileRepository fileRepository;
+	 
 	 FileEntity fileEntity=new FileEntity();
 	 @Test
 	 public void insertFileTest(){
@@ -37,5 +45,27 @@ public class SpringBootTrainingTest {
 	 public void deletFileTest(){
 		 fileService.deletFile(666);
 	 }
-
+	 
+	 /*@SuppressWarnings("static-access")
+	 @Test
+	 public void getAuthUserTest(){
+		 fileManagerController.getAuthUser();
+	 }*/
+	 
+	 long folderId=6666;
+	 int deletFlag= 456;
+	 
+	 //@Test
+	 public void findByFolderIdTest(){
+		 List<FileEntity> list = fileRepository.findByFolderIdAndDeleteFlag(folderId,deletFlag);
+	 }
+	// @Test
+	 public void findByDeleteFlagTest(){
+		 List<FileEntity> list = fileRepository.findByDeleteFlag(deletFlag);
+	 }
+	 @Test
+	 public void findByFileIdTest(){
+		 long id=6666;
+		 List<FileEntity> list = (List<FileEntity>) fileRepository.findByFileId(id);
+	 }
 }
